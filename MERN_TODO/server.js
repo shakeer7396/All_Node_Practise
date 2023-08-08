48a0d6d99ae04c9e461139434fabe0b4c0f4cc8e
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-08-08 11:45:19 +0530

    Update server.js

--- MERN_TODO/server.js
+++ MERN_TODO/server.js
@@ -1,32 +1,3 @@
-// const express = require('express');
-// const mongoose = require("mongoose")
-// const TaskSchema = require("./model.js");
-
-// const app=express();
-
-// mongoose.connect('mongodb+srv://Shaik_Shakeer:shaikshakeer@cluster0.muqcwcm.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true,useNewUrlParser:true}).then(
-//     ()=> console.log('db connected')
-// ).catch(err => console.log(err))
-
-// app.get('/',(req,res)=>{
-//     res.send('<h1>Hello World</h1>');
-// })
-
-// app.post('/addtask',async(req,res)=>{
-//     const {todo} = req.body;
-//     try{
-// const newData = new TaskSchema({
-//     todo: todo
-// })
-// await newData.save();
-// return res.json(await TaskSchema.find())
-//     }
-//     catch(err){
-//         console.log(err.message)
-//     }
-// })
-
-// app.listen(5000,()=>console.log("server running"));
 
 const express = require('express');
 const mongoose = require("mongoose")
@@ -59,5 +30,24 @@ app.post('/addtask', async (req, res) => {
         return res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+app.get("/gettask",async(req,res)=>{
+    try{
+        return res.json(await TaskSchema.find());
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.delete('/delete/:id',async(req,res)=>{
+    try{
+        await TaskSchema.findByIdAndDelete(req.params.id)
+        return res.json(await TaskSchema.find())
+    }
+    catch(err){
+        console.log(err)
+    }
+})
 
 app.listen(5000, () => console.log("server running"));
