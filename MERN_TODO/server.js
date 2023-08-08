dc3c8f72eb91fc100816153b8b9a64e15311bea0
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-08-08 11:11:33 +0530

    post todo

--- MERN_TODO/server.js
+++ MERN_TODO/server.js
@@ -1,29 +1,63 @@
+// const express = require('express');
+// const mongoose = require("mongoose")
+// const TaskSchema = require("./model.js");
+
+// const app=express();
+
+// mongoose.connect('mongodb+srv://Shaik_Shakeer:shaikshakeer@cluster0.muqcwcm.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true,useNewUrlParser:true}).then(
+//     ()=> console.log('db connected')
+// ).catch(err => console.log(err))
+
+// app.get('/',(req,res)=>{
+//     res.send('<h1>Hello World</h1>');
+// })
+
+// app.post('/addtask',async(req,res)=>{
+//     const {todo} = req.body;
+//     try{
+// const newData = new TaskSchema({
+//     todo: todo
+// })
+// await newData.save();
+// return res.json(await TaskSchema.find())
+//     }
+//     catch(err){
+//         console.log(err.message)
+//     }
+// })
+
+// app.listen(5000,()=>console.log("server running"));
+
 const express = require('express');
 const mongoose = require("mongoose")
 const TaskSchema = require("./model.js");
 
-const app=express();
+const app = express();
 
-mongoose.connect('mongodb+srv://Shaik_Shakeer:shaikshakeer@cluster0.muqcwcm.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true,useNewUrlParser:true}).then(
-    ()=> console.log('db connected')
-).catch(err => console.log(err))
+// Middleware to parse JSON in request body
+app.use(express.json());
 
-app.get('/',(req,res)=>{
+mongoose.connect('mongodb+srv://Shaik_Shakeer:shaikshakeer@cluster0.muqcwcm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('db connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
-})
+});
 
-app.post('/addtask',async(req,res)=>{
-    const {todo} = req.body;
-    try{
-const newData = new TaskSchema({
-    todo: todo
-})
-await newData.save();
-return res.json(await TaskSchema.find())
+app.post('/addtask', async (req, res) => {
+    const { todo } = req.body;
+    try {
+        const newData = new TaskSchema({
+            todo: todo
+        });
+        await newData.save();
+        const tasks = await TaskSchema.find();
+        return res.json(tasks);
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-    catch(err){
-        console.log(err.message)
-    }
-})
+});
 
-app.listen(5000,()=>console.log("server running"));
+app.listen(5000, () => console.log("server running"));
